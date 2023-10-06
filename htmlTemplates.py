2fd8902cd06dc07c56d4d9c8c19581ccfa64d376
Sviatoslav <95108031+svybu@users.noreply.github.com>
2023-10-06 21:02:19 +0300

    added templates, run.py

---
+++ htmlTemplates.py
@@ -0,0 +1,44 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://www.stickersplt.com.ua/wp-content/uploads/2020/02/%D0%B1%D0%B5%D0%BD%D0%B4%D0%B5%D1%80.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://pngimg.com/d/futurama_PNG92.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI,Request, UploadFile, File
+from fastapi.templating import Jinja2Templates
 from starlette.middleware.cors import CORSMiddleware
+from typing import List
+import logging
+
 
 from api.routes import auth, healthcheck, user
 
 app = FastAPI(title="LLMExplorer")
+logging.basicConfig(level=logging.INFO)
+
+templates = Jinja2Templates(directory="templates")
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -18,5 +26,7 @@ app.include_router(healthcheck.route)
 app.include_router(auth.router)
 app.include_router(user.router)
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
+@app.get("/")
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "title": "FastAPI with Jinja2", "message": "Hello, World!"})
+
